990a8460059c31e8bde2504c0ce31b2844e19693
Adam Malec <add.malec@gmail.com>
2023-09-28 21:46:21 +0300

    completed: combine objects

---
+++ 7/combine-objects.js
@@ -0,0 +1,13 @@
+function combineObjects(obj1, obj2) {
+  const resultObj = {...obj1};
+
+  for (const key in obj2) {
+    if (Object.hasOwn(resultObj, key)) {
+      resultObj[key] += obj2[key];
+    } else {
+      resultObj[key] = obj2[key];
+    }
+  }
+
+  return resultObj;
+}
